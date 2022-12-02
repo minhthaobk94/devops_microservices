c45d81900fdfc16490e723b58ca662c8b390d84a
Thao Nguyen <minhthaobk94@gmail.com>
2022-12-02 16:25:11 +0700

    Task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="minhthaobk94/mlmapi"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login && docker image tag mlmapi $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
