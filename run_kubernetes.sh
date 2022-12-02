ea254afc1e444a151979adc0fe4726641c8f0fab
Thao Nguyen <minhthaobk94@gmail.com>
2022-12-02 16:55:55 +0700

    Task 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="minhthaobk94/mlmapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run mlmicroserviceapi\
+    --image=$dockerpath\
+    --port=80 --labels app=mlmicroserviceapi
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlmicroserviceapi 8000:80
